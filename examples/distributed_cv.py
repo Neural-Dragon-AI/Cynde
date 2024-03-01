32d6db0a21fb5c283275457363cc225fd6e598cb
furlat <tfurlanello@gmail.com>
2024-03-01 16:05:13 +0100

    Update distributed_cv.py

--- examples/distributed_cv.py
+++ examples/distributed_cv.py
@@ -33,7 +33,7 @@ with datascience_image.imports():
     import cynde.functional as cf
 
 
-def generate_folds_from_np_modal_compatible(cv_df: pl.DataFrame, cv_type: Tuple[str, str], feature_arrays: Dict[str, np.ndarray],
+def generate_folds_from_np_modal_compatible(models: Dict[str, List[Dict[str, Any]]],cv_df: pl.DataFrame, cv_type: Tuple[str, str], feature_arrays: Dict[str, np.ndarray],
                            labels: np.ndarray, group_outer: List[str], k_outer: int, group_inner: List[str], k_inner: int,
                            r_outer: int, r_inner: int) -> Generator:
     for r_o in range(r_outer):
@@ -60,11 +60,12 @@ def generate_folds_from_np_modal_compatible(cv_df: pl.DataFrame, cv_type: Tuple[
                             "test_index": pl.Series(indices_test),
                         }
 
-                        yield (x_tr, y_tr, x_val, y_val, x_te, y_te, fold_meta, feature_name)
+                        yield (models, feature_name, x_tr, y_tr, x_val, y_val, x_te, y_te, fold_meta, )
 
 @stub.function(image=datascience_image)
-def fit_clf_from_np_modal(X_train, y_train, X_val, y_val, X_test, y_test,fold_metadata:dict,
-            classifier: str = "RandomForest", classifier_hp: dict = {}, input_features_name: str = "") -> Tuple[pl.DataFrame, pl.DataFrame]:
+def fit_models_modal(models: Dict[str, List[Dict[str, Any]]], feature_name: str,
+               x_tr: np.ndarray, y_tr: np.ndarray, x_val: np.ndarray, y_val: np.ndarray, x_te: np.ndarray, y_te: np.ndarray,
+               fold_meta: Dict[str, Any]) -> Tuple[List[pl.DataFrame], List[pl.DataFrame]]:
     from sklearn.preprocessing import StandardScaler
     from sklearn.pipeline import make_pipeline
     from sklearn.neighbors import KNeighborsClassifier
@@ -73,87 +74,115 @@ def fit_clf_from_np_modal(X_train, y_train, X_val, y_val, X_test, y_test,fold_me
     from sklearn.metrics import accuracy_score,  matthews_corrcoef
     from sklearn.preprocessing import OneHotEncoder
     import time
-    start_time = time.time()
-    clf = None
-    #create classifiers 
-    if classifier == "RandomForest":
-        clf = RandomForestClassifier(**(classifier_hp or {"random_state": 777, "n_estimators": 100, "max_depth": 5, "n_jobs": -1}))
-    elif classifier == "NearestNeighbors":
-        clf = KNeighborsClassifier(**(classifier_hp or {"n_neighbors": 7}))
-    elif classifier == "MLP":
-        clf = MLPClassifier(**(classifier_hp or {"alpha": 1, "max_iter": 1000, "random_state": 42, "hidden_layer_sizes": (1000, 500)}))
-    else:
-        raise ValueError("Classifier not supported")
+    def fit_clf_from_np_modal(X_train, y_train, X_val, y_val, X_test, y_test,fold_metadata:dict,
+            classifier: str = "RandomForest", classifier_hp: dict = {}, input_features_name: str = "") -> Tuple[pl.DataFrame, pl.DataFrame]:
     
-    #create names 
-    classifier_hp_name = "_".join([f"{key}_{value}" for key,value in classifier_hp.items()])
-    fold_name = fold_metadata["fold_name"]
-    pred_column_name = "{}_{}_{}_{}_y_pred".format(fold_name,input_features_name,classifier,classifier_hp_name)
+        start_time = time.time()
+        clf = None
+        #create classifiers 
+        if classifier == "RandomForest":
+            clf = RandomForestClassifier(**(classifier_hp or {"random_state": 777, "n_estimators": 100, "max_depth": 5, "n_jobs": -1}))
+        elif classifier == "NearestNeighbors":
+            clf = KNeighborsClassifier(**(classifier_hp or {"n_neighbors": 7}))
+        elif classifier == "MLP":
+            clf = MLPClassifier(**(classifier_hp or {"alpha": 1, "max_iter": 1000, "random_state": 42, "hidden_layer_sizes": (1000, 500)}))
+        else:
+            raise ValueError("Classifier not supported")
+        
+        #create names 
+        classifier_hp_name = "_".join([f"{key}_{value}" for key,value in classifier_hp.items()])
+        fold_name = fold_metadata["fold_name"]
+        pred_column_name = "{}_{}_{}_{}_y_pred".format(fold_name,input_features_name,classifier,classifier_hp_name)
 
-    # Train the classifier using the training set
-    start_train_time = time.time()
-    clf = make_pipeline(StandardScaler(), clf)
-    # print("before training")
-    # print("X_train shape: ", X_train.shape)
-    # print("y_train shape: ", y_train.shape)
-    clf.fit(X_train, y_train)
-    end_train_time = time.time()
-    human_readable_train_time = time.strftime("%H:%M:%S", time.gmtime(end_train_time-start_train_time))
+        # Train the classifier using the training set
+        start_train_time = time.time()
+        clf = make_pipeline(StandardScaler(), clf)
+        # print("before training")
+        # print("X_train shape: ", X_train.shape)
+        # print("y_train shape: ", y_train.shape)
+        clf.fit(X_train, y_train)
+        end_train_time = time.time()
+        human_readable_train_time = time.strftime("%H:%M:%S", time.gmtime(end_train_time-start_train_time))
 
-    # Predictions
-    start_pred_time = time.time()
-    # print("before clf predict")
-    y_pred_train = clf.predict(X_train)
-    y_pred_val = clf.predict(X_val)
-    y_pred_test = clf.predict(X_test)
-    end_pred_time = time.time()
-    human_readable_pred_time = time.strftime("%H:%M:%S", time.gmtime(end_pred_time-start_pred_time))
+        # Predictions
+        start_pred_time = time.time()
+        # print("before clf predict")
+        y_pred_train = clf.predict(X_train)
+        y_pred_val = clf.predict(X_val)
+        y_pred_test = clf.predict(X_test)
+        end_pred_time = time.time()
+        human_readable_pred_time = time.strftime("%H:%M:%S", time.gmtime(end_pred_time-start_pred_time))
 
-    # Evaluation
-    start_eval_time = time.time()
-    # print("before evaluation")
-    accuracy_train = accuracy_score(y_train, y_pred_train)
-    accuracy_val = accuracy_score(y_val, y_pred_val)
-    accuracy_test = accuracy_score(y_test, y_pred_test)
-    mcc_train = matthews_corrcoef(y_train, y_pred_train) 
-    mcc_val = matthews_corrcoef(y_val, y_pred_val) 
-    mcc_test = matthews_corrcoef(y_test, y_pred_test) 
-    end_eval_time = time.time()
-    human_readable_eval_time = time.strftime("%H:%M:%S", time.gmtime(end_eval_time-start_eval_time))
-    end_time = time.time()
-    human_readable_total_time = time.strftime("%H:%M:%S", time.gmtime(end_time-start_time))
+        # Evaluation
+        start_eval_time = time.time()
+        # print("before evaluation")
+        accuracy_train = accuracy_score(y_train, y_pred_train)
+        accuracy_val = accuracy_score(y_val, y_pred_val)
+        accuracy_test = accuracy_score(y_test, y_pred_test)
+        mcc_train = matthews_corrcoef(y_train, y_pred_train) 
+        mcc_val = matthews_corrcoef(y_val, y_pred_val) 
+        mcc_test = matthews_corrcoef(y_test, y_pred_test) 
+        end_eval_time = time.time()
+        human_readable_eval_time = time.strftime("%H:%M:%S", time.gmtime(end_eval_time-start_eval_time))
+        end_time = time.time()
+        human_readable_total_time = time.strftime("%H:%M:%S", time.gmtime(end_time-start_time))
 
-    #package predictions
-    pred_train_df = pl.DataFrame({"cv_index":fold_metadata["train_index"],pred_column_name:y_pred_train})
-    pred_val_df = pl.DataFrame({"cv_index":fold_metadata["val_index"],pred_column_name:y_pred_val})
-    pred_test_df = pl.DataFrame({"cv_index":fold_metadata["test_index"],pred_column_name:y_pred_test})
-    pred_df = pred_train_df.vstack(pred_val_df).vstack(pred_test_df)
+        #package predictions
+        pred_train_df = pl.DataFrame({"cv_index":fold_metadata["train_index"],pred_column_name:y_pred_train})
+        pred_val_df = pl.DataFrame({"cv_index":fold_metadata["val_index"],pred_column_name:y_pred_val})
+        pred_test_df = pl.DataFrame({"cv_index":fold_metadata["test_index"],pred_column_name:y_pred_test})
+        pred_df = pred_train_df.vstack(pred_val_df).vstack(pred_test_df)
 
-    #package results
+        #package results
 
-    time_dict = {"train_time":human_readable_train_time,
-                "pred_time":human_readable_pred_time,
-                "eval_time":human_readable_eval_time,"total_cls_time":human_readable_total_time}
+        time_dict = {"train_time":human_readable_train_time,
+                    "pred_time":human_readable_pred_time,
+                    "eval_time":human_readable_eval_time,"total_cls_time":human_readable_total_time}
 
-    results_df = pl.DataFrame({"classifier":[classifier],
-                               "classifier_hp":[classifier_hp_name],
-                               "fold_name":[fold_name],
-                               "pred_name":[pred_column_name],
-                               "input_features_name":[input_features_name],
-                               "accuracy_train":[accuracy_train],
-                                "accuracy_val":[accuracy_val],
-                                "accuracy_test":[accuracy_test],
-                                "mcc_train":[mcc_train],
-                                "mcc_val":[mcc_val],
-                                "mcc_test":[mcc_test],
-                                "train_index":[fold_metadata["train_index"]],
-                                "val_index":[fold_metadata["val_index"]],
-                                "test_index":[fold_metadata["test_index"]],
-                                "time":[time_dict],
-                                }).unnest("time")
+        results_df = pl.DataFrame({"classifier":[classifier],
+                                "classifier_hp":[classifier_hp_name],
+                                "fold_name":[fold_name],
+                                "pred_name":[pred_column_name],
+                                "input_features_name":[input_features_name],
+                                "accuracy_train":[accuracy_train],
+                                    "accuracy_val":[accuracy_val],
+                                    "accuracy_test":[accuracy_test],
+                                    "mcc_train":[mcc_train],
+                                    "mcc_val":[mcc_val],
+                                    "mcc_test":[mcc_test],
+                                    "train_index":[fold_metadata["train_index"]],
+                                    "val_index":[fold_metadata["val_index"]],
+                                    "test_index":[fold_metadata["test_index"]],
+                                    "time":[time_dict],
+                                    }).unnest("time")
+        
+
+        return pred_df, results_df
     
+    pred_list = []
+    results_list = []
 
-    return pred_df, results_df
+    for model, hp_list in models.items():
+        for hp in hp_list:
+            pred_df_col, results_df_row = fit_clf_from_np_modal(x_tr, y_tr, x_val, y_val, x_te, y_te,
+                                                                fold_meta=fold_meta,
+                                                  classifier=model,
+                                                  classifier_hp=hp,
+                                                  input_features_name=feature_name)
+            pred_list.append(pred_df_col)
+            
+            fold_meta_data_df = pl.DataFrame({
+                "k_outer": [fold_meta["k_outer"]],
+                "k_inner": [fold_meta["k_inner"]],
+                "r_outer": [fold_meta["r_outer"]],
+                "r_inner": [fold_meta["r_inner"]]
+            })
+            
+            results_df_row = pl.concat([results_df_row, fold_meta_data_df], how="horizontal") 
+            results_list.append(results_df_row)
+    return pred_list, results_list
+
+
 
 
 
@@ -223,11 +252,12 @@ def main():
 
         # Generate folds and fit models
         folds_generation_start_time = time.time()
-        fit_tasks = generate_folds_from_np_modal_compatible(cv_df, cv_type, feature_arrays, labels, group_outer, k_outer, group_inner, k_inner, r_outer, r_inner)
+        fit_tasks = generate_folds_from_np_modal_compatible(models,cv_df, cv_type, feature_arrays, labels, group_outer, k_outer, group_inner, k_inner, r_outer, r_inner)
         if not skip_class:
-            all_results_list = fit_clf_from_np_modal.starmap(fit_tasks)
+            all_results_list = fit_models_modal.starmap(fit_tasks)
 
-        
+        #flatten the list of lists
+        all_results_list = [item for sublist in all_results_list for item in sublist]
         folds_generation_end_time = time.time()
         print(f"Folds generation and model fitting completed in {folds_generation_end_time - folds_generation_start_time} seconds")
         print(f"Average time per fold: {(folds_generation_end_time - folds_generation_start_time) / (k_outer * k_inner * r_outer * r_inner)}")
