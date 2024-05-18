a29e77f502b85ea2b2b4742035f698c8eac9cc28
furlat <tfurlanello@gmail.com>
2024-05-18 04:42:05 +0200

    added some more metrics estimate and tggate example

--- cynde/functional/train/results.py
+++ cynde/functional/train/results.py
@@ -1,6 +1,8 @@
-from typing import List
+from typing import List, Optional, Tuple, Generator
 from cynde.functional.train.types import PipelineResults
 import polars as pl
+import time
+import os
 
 def merge_results(results:List[PipelineResults]) -> pl.DataFrame:
     """Merge the results of the nested cv into a single dataframe joining over the cv_index column"""
@@ -15,3 +17,25 @@ def merge_metrics(results:List[PipelineResults]) -> pl.DataFrame:
     dfs = [result.to_metrics_df() for result in results]
     metric_df = pl.concat(dfs,how="vertical")
     return metric_df
+
+def aggr_metrics_by_inputs(metrics_df:pl.DataFrame):
+    metrics_columns = ["train_accuracy","train_mcc","val_accuracy","val_mcc","test_accuracy","test_mcc"]
+    return metrics_df.group_by("feature_set_name","classifier_id").agg(pl.mean(metrics_columns),pl.std(metrics_columns).alias([f"{feature}_std" for feature in metrics_columns]),pl.len())
+
+def save_results(results:List[PipelineResults],name_prefix:Optional[str] = None):
+    #extract from first input the save_path
+    save_path = results[0].pipeline_input.input_config.save_folder
+    results_df = merge_results(results)
+    metrics_df = merge_metrics(results)
+    time_stamp = time.strftime("%Y%m%d-%H%M%S")
+    save_name_results = f"results_{time_stamp}.parquet" if name_prefix is None else f"{name_prefix}_results_{time_stamp}.parquet"
+    save_name_metrics = f"metrics_{time_stamp}.parquet" if name_prefix is None else f"{name_prefix}_metrics_{time_stamp}.parquet"
+    save_path_results = os.path.join(save_path,save_name_results)
+    save_path_metrics = os.path.join(save_path,save_name_metrics)
+    print(f"Results saved at {save_path_results}")
+    print(f"Metrics saved at {save_path_metrics}")
+    results_df.write_parquet(save_path_results)
+    metrics_df.write_parquet(save_path_metrics)
+
+    
+  
--- examples/tggate_modal.py
+++ examples/tggate_modal.py
@@ -13,70 +13,58 @@ from sklearn.pipeline import Pipeline
 from cynde.functional.train.train_modal import train_nested_cv_distributed
 from cynde.functional.train.train_local import train_nested_cv
 from cynde.functional.train.preprocess import preprocess_inputs, get_unique_columns
-from cynde.functional.train.results import merge_results
+from cynde.functional.train.results import merge_results, merge_metrics,aggr_metrics_by_inputs, save_results
 
 
 def load_tggate_grouped_data(data_path: str = r"C:\Users\Tommaso\Documents\Dev\Cynde\cache\tgca_nogen_simplified_smiles_malformer_embeddings_ada02_3large_3small_embeddings_grouped.parquet") -> pl.DataFrame:
     return pl.read_parquet(data_path)
+def load_moldes_data(data_path:str = r"C:\Users\Tommaso\Documents\Dev\Cynde\cache\MolDes_3D_TG.tsv") -> pl.DataFrame:
+    return pl.read_csv(data_path, separator="\t")
 
-df = load_tggate_grouped_data()
-df = convert_utf8_to_enum(df, threshold=0.2)
-df = check_add_cv_index(df,strict=False)
+def load_and_prepare_data(data_path_tggate: str = r"C:\Users\Tommaso\Documents\Dev\Cynde\cache\tgca_nogen_simplified_smiles_malformer_embeddings_ada02_3large_3small_embeddings_grouped.parquet",
+                           data_path_moldes:str = r"C:\Users\Tommaso\Documents\Dev\Cynde\cache\MolDes_3D_TG.tsv") -> pl.DataFrame:
+    dft = load_tggate_grouped_data(data_path_tggate)
+    dfm = load_moldes_data(data_path_moldes)
+    dfm = dfm.with_columns(pl.lit(True).alias("in_moldes"))
+    meta_cols = [col for col in dft.columns if "embeddings" not in col]
+    text_embedding_cols = [col for col in dft.columns if "embeddings" in col]
+    moldes_columns = [col for col in dfm.columns if col not in dft.columns]
+    dfj = dft.join(dfm, on="COMPOUND_NAME", how="left")
+    dfj = dfj.filter(~pl.col("in_moldes").is_null())
+    dfjj = dfj.select(pl.col(meta_cols),pl.col(text_embedding_cols),pl.concat_list(moldes_columns).alias("COMPOUND_NAME_moldes_embeddings"))
+    dfjj = (convert_utf8_to_enum(dfjj, threshold=0.2))
+    return check_add_cv_index(dfjj,strict=False)
 
-cols = ['COMPOUND_NAME',
-        'SACRIFICE_PERIOD',
-        'DOSE_LEVEL',
-        'num_samples',
-        'lesion_prob',
-        'weight_mean',
-        'SMILES_CODE_MoLFormer-XL-both-10pct_embeddings',
-        'COMPOUND_NAME_text-embedding-ada-002_embeddings',
-        'SMILES_CODE_text-embedding-ada-002_embeddings',
-        'SACRIFICE_PERIOD_text-embedding-ada-002_embeddings',
-        'DOSE_LEVEL_text-embedding-ada-002_embeddings',
-        'DOSE_DOSE_UNIT_text-embedding-ada-002_embeddings',
-        'COMPOUND_NAME_SACRIFICE_PERIOD_DOSE_LEVEL_text-embedding-ada-002_embeddings',
-        'SMILES_CODE_SACRIFICE_PERIOD_DOSE_LEVEL_text-embedding-ada-002_embeddings',
-        'COMPOUND_NAME_SMILES_CODE_SACRIFICE_PERIOD_DOSE_LEVEL_text-embedding-ada-002_embeddings',
-        'COMPOUND_NAME_SACRIFICE_PERIOD_DOSE_DOSE_UNIT_text-embedding-ada-002_embeddings',
-        'SMILES_CODE_SACRIFICE_PERIOD_DOSE_DOSE_UNIT_text-embedding-ada-002_embeddings',
-        'COMPOUND_NAME_SMILES_CODE_SACRIFICE_PERIOD_DOSE_DOSE_UNIT_text-embedding-ada-002_embeddings',
-        'COMPOUND_NAME_text-embedding-3-small_embeddings', 
-        'SMILES_CODE_text-embedding-3-small_embeddings',
-        'SACRIFICE_PERIOD_text-embedding-3-small_embeddings', 
-        'DOSE_LEVEL_text-embedding-3-small_embeddings',
-        'DOSE_DOSE_UNIT_text-embedding-3-small_embeddings', 
-        'COMPOUND_NAME_DOSE_LEVEL_text-embedding-3-small_embeddings',
-        'SMILES_CODE_DOSE_LEVEL_text-embedding-3-small_embeddings', 
-        'COMPOUND_NAME_SMILES_CODE_DOSE_LEVEL_text-embedding-3-small_embeddings',
-        'COMPOUND_NAME_DOSE_DOSE_UNIT_text-embedding-3-small_embeddings', 
-        'SMILES_CODE_DOSE_DOSE_UNIT_text-embedding-3-small_embeddings',
-        'COMPOUND_NAME_SMILES_CODE_DOSE_DOSE_UNIT_text-embedding-3-small_embeddings', 
-        'COMPOUND_NAME_text-embedding-3-large_embeddings',
-        'SMILES_CODE_text-embedding-3-large_embeddings', 
-        'SACRIFICE_PERIOD_text-embedding-3-large_embeddings', 
-        'DOSE_LEVEL_text-embedding-3-large_embeddings',
-        'DOSE_DOSE_UNIT_text-embedding-3-large_embeddings', 
-        'COMPOUND_NAME_DOSE_LEVEL_text-embedding-3-large_embeddings',
-        'SMILES_CODE_DOSE_LEVEL_text-embedding-3-large_embeddings', 
-        'COMPOUND_NAME_SMILES_CODE_DOSE_LEVEL_text-embedding-3-large_embeddings',
-        'COMPOUND_NAME_DOSE_DOSE_UNIT_text-embedding-3-large_embeddings', 
-        'SMILES_CODE_DOSE_DOSE_UNIT_text-embedding-3-large_embeddings', 
-        'COMPOUND_NAME_SMILES_CODE_DOSE_DOSE_UNIT_text-embedding-3-large_embeddings', 
-        'COMPOUND_NAME_SMILES_CODE_SACRIFICE_PERIOD_DOSE_DOSE_UNIT_text-embedding-3-large_embeddings', 
-        'SMILES_CODE_SACRIFICE_PERIOD_DOSE_DOSE_UNIT_text-embedding-3-large_embeddings', 
-        'COMPOUND_NAME_SACRIFICE_PERIOD_DOSE_DOSE_UNIT_text-embedding-3-large_embeddings', 
-        'COMPOUND_NAME_SMILES_CODE_SACRIFICE_PERIOD_DOSE_LEVEL_text-embedding-3-large_embeddings', 
-        'SMILES_CODE_SACRIFICE_PERIOD_DOSE_LEVEL_text-embedding-3-large_embeddings', 
-        'COMPOUND_NAME_SACRIFICE_PERIOD_DOSE_LEVEL_text-embedding-3-large_embeddings', 
-        'COMPOUND_NAME_SMILES_CODE_SACRIFICE_PERIOD_DOSE_DOSE_UNIT_text-embedding-3-small_embeddings', 
-        'SMILES_CODE_SACRIFICE_PERIOD_DOSE_DOSE_UNIT_text-embedding-3-small_embeddings', 
-        'COMPOUND_NAME_SACRIFICE_PERIOD_DOSE_DOSE_UNIT_text-embedding-3-small_embeddings', 
-        'COMPOUND_NAME_SMILES_CODE_SACRIFICE_PERIOD_DOSE_LEVEL_text-embedding-3-small_embeddings', 
-        'SMILES_CODE_SACRIFICE_PERIOD_DOSE_LEVEL_text-embedding-3-small_embeddings', 
-        'COMPOUND_NAME_SACRIFICE_PERIOD_DOSE_LEVEL_text-embedding-3-small_embeddings', 
-        'target']
+df = load_and_prepare_data()
 
+
+
+
+feature_set_molformer={"embeddings":[{"column_name":"SMILES_CODE_MoLFormer-XL-both-10pct_embeddings",
+                                        "name":"embeddings of the compound names",
+                                        "embedder": "molformer",
+                                        "embedding_size":768},
+                                         {"column_name":"DOSE_LEVEL_text-embedding-3-large_embeddings",
+                                            "name":"embeddings of the dose levels",
+                                            "embedder": "text-embedding-3-large_embeddings",
+                                            "embedding_size":3072},
+                                            {"column_name":"SACRIFICE_PERIOD_text-embedding-3-large_embeddings",
+                                            "name":"embeddings of the sacrifice periods",
+                                            "embedder": "text-embedding-3-large_embeddings",
+                                            "embedding_size":3072}]}
+
+feature_set_moldes={"embeddings":[{"column_name":"COMPOUND_NAME_moldes_embeddings",
+                                        "name":"embeddings of the compound names",
+                                        "embedder": "molder",
+                                        "embedding_size":1826},
+                                         {"column_name":"DOSE_LEVEL_text-embedding-3-large_embeddings",
+                                            "name":"embeddings of the dose levels",
+                                            "embedder": "text-embedding-3-large_embeddings",
+                                            "embedding_size":3072},
+                                            {"column_name":"SACRIFICE_PERIOD_text-embedding-3-large_embeddings",
+                                            "name":"embeddings of the sacrifice periods",
+                                            "embedder": "text-embedding-3-large_embeddings",
+                                            "embedding_size":3072}]}
 
 #the best model combines compound_name, dose_level and sacrifice period embeddings each indipendently using hte large embeddings
 feature_set_best_model = {"embeddings":[{"column_name":"COMPOUND_NAME_text-embedding-3-large_embeddings",
@@ -97,7 +85,7 @@ feature_set_only_compound_name = {"embeddings":[{"column_name":"COMPOUND_NAME_te
                                             "embedder": "text-embedding-3-large_embeddings",
                                             "embedding_size":3072}]}
 
-input_config_data = {"feature_sets":[feature_set_best_model,feature_set_only_compound_name],
+input_config_data = {"feature_sets":[feature_set_molformer,feature_set_moldes,feature_set_best_model,feature_set_only_compound_name],
                         "target_column":"target",
                         "save_folder":"C:/Users/Tommaso/Documents/Dev/Cynde/cynde_mount/",
                         "remote_folder":"/cynde_mount"}
@@ -110,9 +98,9 @@ inner_groups = ["DOSE_LEVEL","SACRIFICE_PERIOD","target"]
 outer_groups = ["COMPOUND_NAME"]
 
 cv_config = CVConfig(inner= StratifiedConfig(groups=inner_groups,k=5),
-                     inner_replicas=1,
+                     inner_replicas=5,
                      outer = PurgedConfig(groups=outer_groups,k=5),
-                        outer_replicas=1)
+                        outer_replicas=10)
 
 task = PredictConfig(input_config=input_config, cv_config=cv_config, classifiers_config=classifiers_config)
 
@@ -127,6 +115,8 @@ preprocess_inputs(df_filtered,task.input_config)
 # results = train_nested_cv(df,task)
 
 results = train_nested_cv_distributed(df_filtered,task)
+save_results(results,"tgate_with_moldes")
 merged_results = merge_results(results)
-print(merged_results)
-
+merged_metrics = merge_metrics(results)
+aggr_metrics = aggr_metrics_by_inputs(merged_metrics)
+print(aggr_metrics)
